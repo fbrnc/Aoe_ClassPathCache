427591ac20997d266880aef631302094df143962
Fabrizio Branca <fabrizio.branca@aoemedia.de>
2013-05-23 15:10:44 -0700

    Added helper, controller and shell script to clear classpathcache from every situation

---
+++ app/code/local/Aoe/ClassPathCache/Helper/Data.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * Helper
+ * 
+ * @author Fabrizio Branca
+ * @since 2013-05-23
+ */
+class Aoe_ClassPathCache_Helper_Data extends Mage_Core_Helper_Abstract {
+
+    /**
+     * Clear the class path cache
+     *
+     * @return bool
+     */
+    public function clearClassPathCache() {
+        $result = false;
+        if (Varien_Autoload::isApcUsed()) {
+            if (php_sapi_name() == 'cli') {
+                // do frontend call
+                Mage::log('[ClassPathCache] Doing frontend call.');
+                $response = file_get_contents($this->getUrl());
+                if ($response != 'OK') {
+                    $result = false;
+                    Mage::log('[ClassPathCache] Frontend call failed. Response: ' . $response);
+                } else {
+                    $result = true;
+                    Mage::log('[ClassPathCache] Frontend call success');
+                }
+            } else {
+                // delete cache directly
+                $result = !apc_exists(Varien_Autoload::getCacheKey()) || apc_delete(Varien_Autoload::getCacheKey());
+                if ($result) {
+                    Mage::log('[ClassPathCache] Delete cache from apc.');
+                } else {
+                    Mage::log('[ClassPathCache] Deleting cache from apc FAILED.');
+                }
+            }
+        } else {
+            $result = unlink(Varien_Autoload::getCacheFilePath());
+            if ($result) {
+                Mage::log('[ClassPathCache] Delete cache from file system.');
+            } else {
+                Mage::log('[ClassPathCache] Deleting cache from file system FAILED.');
+            }
+        }
+        return $result;
+    }
+
+    /**
+     * Check url
+     *
+     * @return bool
+     */
+    public function checkUrl()
+    {
+        $k = Mage::app()->getRequest()->getParam('k');
+        $v = Mage::app()->getRequest()->getParam('v');
+        $ek = Mage::helper('core')->decrypt($v);
+        return $k && $v && ($ek == $k);
+    }
+
+    /**
+     * Check url
+     *
+     * @return bool
+     */
+    public function getUrl()
+    {
+        $k = Mage::helper('core')->getRandomString(16);
+        return Mage::getUrl('aoeclasspathcache/index/clear', array(
+            'k' => $k,
+            'v' => Mage::helper('core')->encrypt($k),
+            '_store' => 'default' // TODO: that's not nice
+        ));
+    }
+
+
+}
---
+++ shell/aoe_classpathcache.php
@@ -0,0 +1,79 @@
+<?php
+
+require_once 'abstract.php';
+
+/**
+ * AOE ClassPathCache CLI
+ *
+ * @author Fabrizio Branca
+ * @since 2013-05-23
+ */
+class Aoe_ClassPathCache_Shell extends Mage_Shell_Abstract {
+
+    /**
+     * Clear
+     */
+    public function clearAction() {
+        if (Mage::helper('aoe_classpathcache')->clearClassPathCache()) {
+            echo "Success\n";
+        } else {
+            echo "Error while cleaning class path cache. Check system.log for details\n";
+            exit(1);
+        }
+    }
+
+
+	/** ****************************************************************************************************************
+	 * SHELL DISPATCHER
+	 **************************************************************************************************************** */
+
+	/**
+	 * Run script
+	 *
+	 * @return void
+	 */
+	public function run() {
+		$action = $this->getArg('action');
+		if (empty($action)) {
+			echo $this->usageHelp();
+		} else {
+			$actionMethodName = $action.'Action';
+			if (method_exists($this, $actionMethodName)) {
+				$this->$actionMethodName();
+			} else {
+				echo "Action $action not found!\n";
+				echo $this->usageHelp();
+				exit(1);
+			}
+		}
+	}
+
+
+
+	/**
+	 * Retrieve Usage Help Message
+	 *
+	 * @return string
+	 */
+	public function usageHelp() {
+		$help = 'Available actions: ' . "\n";
+		$methods = get_class_methods($this);
+		foreach ($methods as $method) {
+			if (substr($method, -6) == 'Action') {
+				$help .= '    -action ' . substr($method, 0, -6);
+				$helpMethod = $method.'Help';
+				if (method_exists($this, $helpMethod)) {
+					$help .= $this->$helpMethod();
+				}
+				$help .= "\n";
+			}
+		}
+		return $help;
+	}
+
+
+}
+
+$shell = new Aoe_ClassPathCache_Shell();
+$shell->run();
+
