e4a9b26b50f5e990aae76622d7f41060a4818661
Lee Saferite <lee.saferite@aoe.com>
2014-05-30 10:55:19 -0400

    Adding PSR-0 autoloader namespace handling

--- app/code/local/Varien/Autoload.php
+++ app/code/local/Varien/Autoload.php
@@ -96,7 +96,18 @@ class Varien_Autoload
      * @return string
      */
     static function getFileFromClassName($className) {
-        return str_replace(' ', DIRECTORY_SEPARATOR, ucwords(str_replace('_', ' ', $className))) . '.php';
+        $className = ltrim($className, '\\');
+        $fileName  = '';
+        $namespace = '';
+        if ($lastNsPos = strrpos($className, '\\')) {
+            $namespace = substr($className, 0, $lastNsPos);
+            $className = substr($className, $lastNsPos + 1);
+            $fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+        }
+
+        $fileName .= str_replace(' ', DIRECTORY_SEPARATOR, ucwords(str_replace('_', ' ', $className))) . '.php';
+
+        return $fileName;
     }
 
     /**
